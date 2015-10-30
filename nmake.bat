fcac3a0964a392b59c880d08022a0ca37b88b316
cygx <cygx@cpan.org>
2015-10-30 12:24:58 +0100

    add /nologo to cl invocation

--- nmake.bat
+++ nmake.bat
@@ -1,1 +1,1 @@
-@nmake.exe /NOLOGO MKDIR="build\loopify.bat md" RM="build\loopify.bat del" MV="move" INSTALL="build\install.bat" DLLFLAGS="/link /DLL" DLLEXT="dll" CC="cl" CFLAGS="" OUT="/OUT:" GARBAGE="p6-native-libc.obj p6-native-libc.lib p6-native-libc.exp" %*
+@nmake.exe /nologo MKDIR="build\loopify.bat md" RM="build\loopify.bat del" MV="move" INSTALL="build\install.bat" DLLFLAGS="/link /DLL" DLLEXT="dll" CC="cl /nologo" CFLAGS="" OUT="/OUT:" GARBAGE="p6-native-libc.obj p6-native-libc.lib p6-native-libc.exp" %*
